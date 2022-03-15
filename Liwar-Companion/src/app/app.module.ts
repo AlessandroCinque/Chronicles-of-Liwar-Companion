8f60a00f5ab50208c7a8a4da8933e6863117ec24
AlessandroCinque <alessandro5cinque@gmail.com>
2022-03-15 17:46:51 +0000

    Still at the very beginning

--- Liwar-Companion/src/app/app.module.ts
+++ Liwar-Companion/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarbandPanelComponent } from './warband-panel/warband-panel.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarbandPanelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Liwar-Companion/src/app/warband-panel/warband-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warband-panel',
+  templateUrl: './warband-panel.component.html',
+  styleUrls: ['./warband-panel.component.css']
+})
+export class WarbandPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
