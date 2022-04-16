f57ecd404284f8cb392680437f83f57a1d00eb0a
AlessandroCinque <alessandro5cinque@gmail.com>
2022-04-16 17:23:39 +0100

    don't know what is going on

--- Liwar-Companion/src/app/Members/members.ts
+++ Liwar-Companion/src/app/Members/members.ts
@@ -1,8 +1,64 @@
 export class Members 
 {
     name: string= ""
+    AF: number = 0
+    AM: number = 0
+    M: number = 0
+    Spare: number = 14
+    MAX_Spare:number = 14
+
     constructor(name: string)
     {
         this.name = name
     }
+    //ALL the Setters work BOTH WAYS
+
+
+    Set_AF(value:number)
+    {
+        if (value > 0)
+        {
+            this.Spare = this.Spare - 1
+
+            if(this.Spare >= 0)// Maybe AF has also a max value
+            {
+                this.AF += 1
+            }
+            else
+            {
+                this.Spare = 0
+            }
+
+        }
+        else
+        {
+            this.Spare += 1
+
+            if(this.Spare <= this.MAX_Spare)
+            {
+                this.AF = this.AF -1
+            }
+            else
+            {
+                this.Spare = this.MAX_Spare
+            }
+        }
+        
+       
+    }
+    Set_AM(value:number)
+    {
+       
+    }
+    Set_M(value:number)
+    {
+
+        this.M += value
+    }
+
+    Get_Spare()
+    {
+        return this.Spare
+    }
+
 }
